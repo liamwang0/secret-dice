eee86e467b886603193222f445c335bce8364035
liamwang0 <hadqeeeulh7601@hotmail.com>
2025-10-23 11:54:18 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedSecretDice = await deploy("SecretDice", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`SecretDice contract: `, deployedSecretDice.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_secret_dice"; // id required to prevent reexecution
+func.tags = ["SecretDice"];
